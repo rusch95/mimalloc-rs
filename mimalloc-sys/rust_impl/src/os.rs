4a9b002fe355c46cdc8e48b0e5c71c41b4c11216
Robert Rusch <rusch@mit.edu>
2019-06-24 23:38:15 -0400

    Switch to C2Rust conversion for first porting.

--- mimalloc-sys/rust_impl/src/os.rs
+++ mimalloc-sys/rust_impl/src/os.rs
@@ -2,15 +2,7 @@ use libc::*;
 
 #[no_mangle]
 pub extern "C" fn _mi_align_up_rs(sz: uintptr_t, alignment: size_t) -> uintptr_t {
-    let x = (sz / alignment) * alignment;
-    if x < sz { 
-        let (x, overflowed) = x.overflowing_add(alignment); 
-        if overflowed {
-            0
-        } else {
-            x
-        }
-    } else {
-        x
-    }
+    let mut x = sz.wrapping_div(alignment).wrapping_mul(alignment);
+    if x < sz { x = x.wrapping_add(alignment) };
+    if x < sz { 0 } else { x }
 }
